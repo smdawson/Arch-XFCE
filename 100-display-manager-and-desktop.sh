45090bce706b445454eef0d867e652dea4d140c2
smdawson <seth.dawson@hotmail.com>
2020-06-03 09:55:01 -0500

    Remove old info

--- 100-display-manager-and-desktop.sh
+++ 100-display-manager-and-desktop.sh
@@ -21,24 +21,6 @@ echo "#     Installing Display Manager and Desktop     #"
 echo "#                                                #"
 echo "##################################################"
 
-sudo pacman -Syyu --noconfirm
-#installing displaymanager or login manager
-sudo pacman -S --noconfirm --needed lightdm smd-arch-lightdm-gtk-greeter smd-arch-lightdm-gtk-greeter-settings smd-arch-wallpapers-git
-#installing desktop environment
-sudo pacman -S xfce4 xfce4-goodies --noconfirm --needed
-#enabling displaymanager or login manager
-sudo systemctl enable lightdm.service -f
-sudo systemctl set-default graphical.target
-
-#remove xfce stuff
-sudo pacman -R xfce4-artwork xfce4-screensaver --noconfirm
-
-echo "##################################################"
-echo "#                                                #"
-echo "#  Display Manager and Desktop Install Complete  #"
-echo "#                                                #"
-echo "##################################################"
-
 ###############################################################################
 #
 #   DECLARATION OF FUNCTIONS
